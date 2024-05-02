ea9d9cfc92a6e34dd9a2e350f31b066ca97cb6c0
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-05-02 13:04:45 +0530

    Update file

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import SideBar from "./Sidebar";
 import MainContainer from "./MainContainer";
+import SideBar from "./SideBar";
 
 const Body = () => {
   return (
--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const VideoContainer = () => {
   return <div>VideoContainer</div>;
 };
