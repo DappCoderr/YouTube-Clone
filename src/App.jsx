883b631a0e8853756399b58b8b1f6f94fb7953fc
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-05-03 12:58:52 +0530

    Add: App slice state using Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { Provider } from "react-redux";
 import Body from "./components/Body";
 import Header from "./components/Header";
+import store from "../utils/store";
 
 export default function App() {
   return (
-    <div>
-      <Header />
-      <Body />
-    </div>
+    <Provider store={store}>
+      <div>
+        <Header />
+        <Body />
+      </div>
+    </Provider>
   );
 }
