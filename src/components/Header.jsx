77d75351d4d3576c3d79694392f72cc7b50534fe
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-05-03 15:45:40 +0530

    Add: working with the hamburger, using redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,23 @@
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../../utils/appSlice";
+
 const Header = () => {
+  const dispatch = useDispatch();
+  function handleMenuToggle() {
+    dispatch(toggleMenu());
+  }
+
   return (
     <div className="grid w-full place-items-center h-16 grid-flow-col p-2 shadow-lg">
       <div className="flex col-span-1">
         <img
-          className="h-8"
+          className="h-8 cursor-pointer"
           src="https://cdn3.iconfinder.com/data/icons/e-commerce-8/91/hamburger-512.png"
           alt="menu-icon"
+          onClick={handleMenuToggle}
         />
         <img
-          className="h-8 ml-4"
+          className="h-8 ml-4 cursor-pointer"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1200px-YouTube_Logo_2017.svg.png"
           alt="youtube-logo"
         />
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,26 +1,35 @@
+import { useSelector } from "react-redux";
+
 const SideBar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
+  // Early return pattern
+  if (!isMenuOpen) return null;
+
   return (
-    <div className="shadow-lg col-span-1 pl-6 h-screen border">
-      <ul className="my-2">
-        <li>Home</li>
-        <li>Shorts</li>
-        <li>Subscription</li>
+    <div className="shadow-lg col-span-1 h-screen border">
+      <ul className="my-2 pl-4">
+        <li className="cursor-pointer py-2">Home</li>
+        <li className="cursor-pointer py-2">Shorts</li>
+        <li className="cursor-pointer py-2">Subscription</li>
       </ul>
-      <h1 className="font-bold mb-2">You</h1>
-      <ul>
-        <li>History</li>
-        <li>Playlists</li>
-        <li>Watch Later</li>
-        <li>Liked Videos</li>
+      <div className="border my-2"></div>
+      <h1 className="font-bold mb-2 pl-4">You</h1>
+      <ul className="pl-4">
+        <li className="cursor-pointer py-2">History</li>
+        <li className="cursor-pointer py-2">Playlists</li>
+        <li className="cursor-pointer py-2">Watch Later</li>
+        <li className="cursor-pointer py-2">Liked Videos</li>
       </ul>
-      <h1 className="font-bold mb-2">Explore</h1>
-      <ul>
-        <li>Trending</li>
-        <li>Shopping</li>
-        <li>Music</li>
-        <li>Live</li>
-        <li>Gaming</li>
-        <li>News</li>
+      <div className="border my-2"></div>
+      <h1 className="font-bold mb-2 pl-4 py-2">Explore</h1>
+      <ul className="pl-4">
+        <li className="cursor-pointer py-2">Trending</li>
+        <li className="cursor-pointer py-2">Shopping</li>
+        <li className="cursor-pointer py-2">Music</li>
+        <li className="cursor-pointer py-2">Live</li>
+        <li className="cursor-pointer py-2">Gaming</li>
+        <li className="cursor-pointer py-2">News</li>
       </ul>
     </div>
   );
