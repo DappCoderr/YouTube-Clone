893bf7a5e406f2296eb62e4b69327546dedf8f5f
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-05-04 20:58:51 +0530

    Dispatch sidebar action

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
 
 const WatchPage = () => {
-  return <div>WatchPage</div>;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
+
+  return <div className="col-span-11">WatchPage</div>;
 };
 
 export default WatchPage;
